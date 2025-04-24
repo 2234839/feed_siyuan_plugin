e7a074a017fc2e6da05e69807a8fff52faa1e91c
崮生 <admin@shenzilong.cn>
2025-04-24 07:13:24 +0000

    Update dist to GitHub

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import { Plugin, fetchPost } from "siyuan";
-import { SiyuanPlugin } from "~/libs/siyuanPlugin";
+import { Plugin, fetchPost } from 'siyuan';
+import { SiyuanPlugin } from '~/libs/siyuanPlugin';
 // 引入这个变量后 vite 会自动注入 hot
 import.meta.hot;
 
-
-import { removeAllCronJob, scheduleCronJob } from "./libs/cron";
-import { getAllFeedBlocks, parseFeedBlock, parseFeed, linkFilter } from "./libs/feed";
-import { DEFAULT_CRON } from "./libs/const";
-import { insertBlock } from "./libs/siyuan_api";
+import { removeAllCronJob, scheduleCronJob } from './libs/cron';
+import { getAllFeedBlocks, parseFeedBlock, parseFeed, linkFilter } from './libs/feed';
+import { DEFAULT_CRON } from './libs/const';
+import { insertBlock } from './libs/siyuan_api';
+import { getBlockByID } from '~/libs/api';
 
 export default class FeedPlugin extends SiyuanPlugin {
   /** 拉取feed链接并进行解析的函数 */
   _feedFetch: (() => void)[] = [];
   async onload() {
     this.addCommand({
-      hotkey: "",
-      langKey: "_feedFetch",
-      langText: "立刻对所有feed进行一次拉取",
+      hotkey: '',
+      langKey: '_feedFetch',
+      langText: '立刻对所有feed进行一次拉取',
       callback: async () => {
         await this.registerAllFeed();
         this._feedFetch.forEach((feedFetch) => feedFetch());
@@ -33,16 +33,16 @@ export default class FeedPlugin extends SiyuanPlugin {
     return Promise.all(
       feedBlocks.map(async (block) => {
         const feedDoc = await parseFeedBlock(block.block_id);
-        if (feedDoc.getAttr("feed")) {
-          const cron = feedDoc.getAttr("cron") ?? DEFAULT_CRON;
-          console.log(`注册 cron job 表达式:${cron} by ${feedDoc.getAttr("feed")}`, feedDoc);
+        if (feedDoc.getAttr('feed')) {
+          const cron = feedDoc.getAttr('cron') || DEFAULT_CRON;
+          console.log(`注册 cron job 表达式:${cron} by ${feedDoc.getAttr('feed')}`, feedDoc);
           const feedFetch = async () => {
             this.feedFetch(block.block_id);
           };
           scheduleCronJob(cron, feedFetch);
           this._feedFetch.push(feedFetch);
         } else {
-          console.log(feedDoc, "没有读取到 feed 属性，请对照文档进行设定 feed");
+          console.log(feedDoc, '没有读取到 feed 属性，请对照文档进行设定 feed');
         }
       }),
     );
@@ -66,28 +66,37 @@ export default class FeedPlugin extends SiyuanPlugin {
            * TODO 如果有更新的话应该也要再次处理
            */
           (entryBlock) =>
-            el.link && entryBlock.content.split("\n")[0].includes(linkFilter(el.link)),
+            el.link && entryBlock.content.split('\n')[0].includes(linkFilter(el.link)),
         );
         return s;
       });
-    const msg = `feed:${feedDoc.getAttr("feed")} 共 ${feed.entryList.length} 条，新增 ${
+    const msg = `feed:${feedDoc.getAttr('feed')} 共 ${feed.entryList.length} 条，新增 ${
       insertEntry.length
     } 条`;
     console.log(msg);
-    fetchPost("/api/notification/pushMsg", {
+    fetchPost('/api/notification/pushMsg', {
       msg,
     });
+
+    const feedBlockId = feedDoc.attrBlock?.id ?? feedId;
+
+    const block = await getBlockByID(feedBlockId);
     insertEntry.forEach(async (entry) => {
-      console.log("insertBlock ", entry);
-      const previousID = feedDoc.attrBlock?.id ?? feedId;
       let data = `* [ ] ###### [${entry.title ?? entry.link}](${entry.link})\n`;
       if (entry.published) data += `    - published:${entry.published}\n`;
       if (entry.updated) data += `    - updated:${entry.updated}\n`;
       if (entry.summary) data += `    > ${entry.summary}\n`;
       data += `  `;
       insertBlock({
-        dataType: "markdown",
-        previousID,
+        dataType: 'markdown',
+        ...(block.type === 'd'
+          ? {
+              parentID: feedBlockId,
+            }
+          : {
+              previousID: feedBlockId,
+            }),
+
         data,
       });
     });
